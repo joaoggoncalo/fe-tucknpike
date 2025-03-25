9cc07ea5dcc504501ef99cff7c3584e3bf42031a
joaoggoncalo <jppjgp@gmail.com>
2025-03-25 15:26:48 +0100

    show different screens for different roles (#7)

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:fe_tucknpike/routes.dart';
+import 'package:fe_tucknpike/services/auth_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load();
+  await AuthService().init(); // Initialize auth service to load stored token.
   runApp(const MyApp());
 }
 
